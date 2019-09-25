8361ce35d46d901f0931281e750c284d8c665008
Chantel Lowe <chantellowe17@gmail.com>
2019-09-25 09:04:32 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,7 +2,13 @@ module Memorable
   
   module ClassMethods
     
-    
+    def self.reset_all
+      self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end
     
   end
   
